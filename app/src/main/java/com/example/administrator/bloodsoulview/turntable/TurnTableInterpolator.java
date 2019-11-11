71139f8fa88e3a746e78522e7938dc7cde228165
bloodsoul <812566160@qq.com>
2019-11-11 19:10:48 +0800

    commit

--- app/src/main/java/com/example/administrator/bloodsoulview/turntable/TurnTableInterpolator.java
+++ app/src/main/java/com/example/administrator/bloodsoulview/turntable/TurnTableInterpolator.java
@@ -1,5 +1,6 @@
 package com.example.administrator.bloodsoulview.turntable;
 
+import android.util.Log;
 import android.view.animation.BaseInterpolator;
 
 /**
@@ -35,12 +36,32 @@ public class TurnTableInterpolator extends BaseInterpolator {
          *  0.38 - 1 匀减速
          */
 
+//        if (input < 0.18f) {
+//            return (float) (Math.pow(input, 2) / 0.216f);
+//        } else if (input < 0.38f) {
+//            return 0.15f + (input - 0.18f) / 0.6f;
+//        } else {
+//            return (float) (0.15f + 1f / 3f + (input - 0.38f) / 0.6f - Math.pow(input - 0.38f, 2) / 0.744f);
+//        }
+
+        /*
+         *  时间度
+         *  0 - 0.18 匀加速 0.15 路程
+         *  0.18 - 0.38 匀速 1/3 路程
+         *  0.38 - 0.68 匀减速 v/6
+         *  0.68 - 1 匀减速
+         */
         if (input < 0.18f) {
-            return (float) (Math.pow(input, 2) / 0.216f);
+            return (float) (Math.pow(input, 2) * 1000f / 177f);
         } else if (input < 0.38f) {
-            return 0.15f + (input - 0.18f) / 0.6f;
+            return (float) ((Math.pow(0.18f, 2) * 1000f / 177f) + (input - 0.18f) * 120f / 59f);
+        } else if (input < 0.68f) {
+            return (float) ((Math.pow(0.18f, 2) * 1000f / 177f) + (0.38f - 0.18f) * 120f / 59f + 120f / 59f
+                    * (input - 0.38f) - 1500f / 531f * Math.pow(input - 0.38f, 2));
         } else {
-            return (float) (0.15f + 1f / 3f + (input - 0.38f) / 0.6f - Math.pow(input - 0.38f, 2) / 0.744f);
+            return (float) ((Math.pow(0.18f, 2) * 1000f / 177f) + (0.38f - 0.18f) * 120f / 59f + 120f / 59f
+                    * (0.68f - 0.38f) - 1500f / 531f * Math.pow(0.68f - 0.38f, 2) + 20f / 59f *
+                    (input - 0.68f) - 125f / 236f * Math.pow(input - 0.68f, 2));
         }
     }
 
