25a1e8c57a167d439420bba4ae60e9599fc7f014
bloodsoul <812566160@qq.com>
2019-12-02 17:18:16 +0800

    commit

--- app/src/main/java/com/example/administrator/bloodsoulview/lottie/LottieActivity.java
+++ app/src/main/java/com/example/administrator/bloodsoulview/lottie/LottieActivity.java
@@ -1,15 +1,44 @@
 package com.example.administrator.bloodsoulview.lottie;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.example.administrator.bloodsoulview.R;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LottieActivity extends AppCompatActivity {
+
+    @BindView(R.id.img)
+    ImageView mImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lottie);
+        ButterKnife.bind(this);
+
+        Bitmap bitmap = loadAssetsBitmap();
+        mImg.setImageBitmap(bitmap);
+    }
+
+    private Bitmap loadAssetsBitmap() {
+        Bitmap bitmap = null;
+        AssetManager assetManager = this.getAssets();
+        try {
+            InputStream inputStream = assetManager.open("emoji/img.png"); //filename是assets目录下的图片名
+            bitmap = BitmapFactory.decodeStream(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 }
